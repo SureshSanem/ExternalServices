9b987b5ed59b57a62b8398214e05c571eb37dcc8
Suresh Sanem <suresh.sanem@xpo.com>
2025-02-02 22:59:12 +0530

    initial commit

---
+++ src/main/java/com/jocata/externalservices/ExternalservicesApplication.java
@@ -0,0 +1,13 @@
+package com.jocata.externalservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jocata/externalservices/ExternalservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jocata.externalservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
